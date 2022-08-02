4164b668b8ce7e1521d86e6d568b1465aedc25d2
biantris <anabeatrizxoliveira@gmail.com>
2022-08-02 08:35:56 -0300

    fix(test-ci): remove dotenv

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,3 @@
-import dotenvSafe from "dotenv-safe";
-import path from "path";
-
-const cwd = process.cwd();
-
-const root = path.join.bind(cwd);
-
-dotenvSafe.config({
-  path: root(".env"),
-  sample: root(".env.example"),
-  allowEmptyValues: true,
-});
-
 export const config = {
   PORT: process.env.PORT || 5000,
   NODE_ENV: process.env.NODE_ENV,

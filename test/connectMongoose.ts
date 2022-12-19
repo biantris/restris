5bcd469917188e23b8c2ae96104634b2dc5ca2b6
Beatriz Oliveira <anabeatrizxoliveira@gmail.com>
2022-12-19 09:13:29 -0300

    feat(up): update all

--- test/connectMongoose.ts
+++ test/connectMongoose.ts
@@ -1,17 +1,19 @@
 import mongoose from 'mongoose';
 
-const mongooseOptions = {
-  autoIndex: false,
-  autoReconnect: false,
-  connectTimeoutMS: 10000,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-};
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      __MONGO_URI__: string;
+      __MONGO_DB_NAME__: string;
+    }
+  }
+}
 
 export async function connectMongoose() {
   jest.setTimeout(20000);
   return mongoose.connect(global.__MONGO_URI__, {
-    ...mongooseOptions,
+    connectTimeoutMS: 10000,
     dbName: global.__MONGO_DB_NAME__,
   });
 }

3d83b32576b9803a517b07a73d9f1947cf55f89e
Beatriz Oliveira <anabeatrizxoliveira@gmail.com>
2022-12-19 08:58:10 -0300

    fix config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   PORT: process.env.PORT || 9000,
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/physio-back',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/restris',
   JWT_SECRET: process.env.JWT_KEY || 'secret_key',
 };
